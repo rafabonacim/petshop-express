a99ecae380a0150031b337ea8177f65e0936de62
rafaelabonacim <rafaelabonacim@gmail.com>
2021-05-29 01:47:06 -0300

    inclusão do erro 404

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,10 +20,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
+
+//erro
+app.use((req,res)=>{
+  return res.status(404).render('not-found');
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
